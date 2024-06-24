40945ff6ac87c7e61e390d56de56bdbab240961b
Brenda <113529315+Brendaur11@users.noreply.github.com>
2024-06-24 17:00:20 -0300

    Add files via upload

---
+++ assets/components/experiences.js
@@ -0,0 +1,49 @@
+const EXPERIENCES = [
+    {
+        tittle: "Help Desk Support",
+        company: "JLA Argentina",
+        description: "I work in technical support for users; managing incidents and requests through a ticketing tool; creating guides; user account setup, modification, and termination.",
+        time: "At present"
+    },
+    {
+        tittle: "Dependiente de Aduana",
+        company: "Ines Verstraete",
+        description: "Management of export and import documents. Continuous communication and interaction with customs.",
+        time: "2020 - 2023",
+    },
+];
+
+document.addEventListener('DOMContentLoaded', () => {
+    const experiences = document.getElementById('experiencesInfo');
+
+    function crearTimeline() {
+        const timeline = document.createElement('ol');
+        timeline.classList = 'relative mt-16';
+
+        EXPERIENCES.forEach(exp => {
+            const li = document.createElement('li');
+            li.classList = 'ms-4 ml-8';
+            li.innerHTML = `
+                <div class="relative mx-12 pb-12 grid before:absolute before:left-[-35px] before:block before:h-full before:border-l-2 before:border-white/20 dark:before:border-white/15 before:content-[''] md:grid-cols-5 md:gap-10 md:space-x-4]">
+                    <div class="relative pb-12 md:col-span-2">
+                        <div class="sticky top-0">
+                            <span class="text-blue-400 -left-[44px] absolute rounded-full text-5xl">•</span>
+                            <h3 class="text-xl font-semibold text-blue-300 dark:text-white">${exp.tittle}</h3>
+                            <p class="mb-1 text-base font-normal text-gray-200 dark:text-gray-200">${exp.company}</p>
+                            <time class="mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">${exp.time}</time>
+                        </div>
+                    </div>
+                    <div class="relative flex flex-col gap-2 pb-4 text-gray-300 dark:text-gray-300 md:col-span-3">
+                        ${exp.description}
+                    </div>
+                </div>
+                
+            `;
+            timeline.appendChild(li);
+        });
+
+        experiences.appendChild(timeline);
+    }
+
+    crearTimeline();
+});
---
+++ assets/components/tarjetasPortfolio.js
@@ -0,0 +1,54 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const portfolioContainer = document.getElementById("tarjetasPortfolio");
+    const radioButtons = document.querySelectorAll('input[name="category"]');
+
+    function crearTarjetasPortfolio(proyectos) {
+        portfolioContainer.innerHTML = ''; // Limpiar el contenedor antes de añadir nuevas tarjetas
+        proyectos.forEach(proyecto => {
+            const nuevoProyecto = document.createElement("div");
+            nuevoProyecto.classList = "tarjeta lg:w-3/4 md:w-3/4 p-4 w-full mt-8 mx-auto"; // Asegúrate de que la clase 'tarjeta' se añade aquí
+            nuevoProyecto.innerHTML = `
+            <div class="flex flex-col space-x-0 space-y-8 group md:flex-row md:space-x-8 md:space-y-0">
+                <div class="rounded-lg h-62 w-3/5 overflow-hidden">
+                    <img alt="content" class="object-cover object-center h-full w-full" src="${proyecto.imagen}">
+                </div>
+                <div class="p-4 md:w-3/5 sm:mb-0 mb-6">
+                    <h2 class="text-xl font-medium title-font text-gray-200 mt-5">${proyecto.titulo}</h2>
+                    <h3 class="text-gray-400 text-xs tracking-widest title-font mb-1">${proyecto.categoria}</h3>
+                    <p class="text-base text-gray-400 leading-relaxed mt-2">${proyecto.description}</p>
+                    <div class="buttons flex flex-wrap">
+                        ${proyecto.links ? proyecto.links.map(link => `<a href="${link.href}"><button class="btnPortfolio mr-2"><i class='${link.icon}' style='color:#ffffff'></i></button></a>`).join('') : `<a href="${proyecto.link}"><button class="btnPortfolio mr-2"><i class='${proyecto.icon}' style='color:#ffffff'></i></button></a>`}
+                    </div>
+                </div>
+             </div>   
+            `;
+            portfolioContainer.appendChild(nuevoProyecto);
+        });
+    }
+
+
+    
+
+    function filtrarTarjetasPorCategoria(categoria) {
+        const tarjetas = document.querySelectorAll('.tarjeta');
+        console.log("Categoría seleccionada:", categoria);
+        tarjetas.forEach(tarjeta => {
+            const categoriaTarjeta = tarjeta.querySelector('.text-gray-400').textContent.trim().toLowerCase();
+            if (categoria.toLowerCase() === categoriaTarjeta) {
+                tarjeta.style.display = 'block'; // Mostrar la tarjeta si coincide con la categoría seleccionada
+            } else {
+                tarjeta.style.display = 'none'; // Ocultar la tarjeta si no coincide con la categoría seleccionada
+            }
+        });
+    }
+
+    // Evento para manejar el cambio de categoría
+    radioButtons.forEach(radio => {
+        radio.addEventListener('change', (event) => {
+            filtrarTarjetasPorCategoria(event.target.value);
+        });
+    });
+
+    crearTarjetasPortfolio(proyectos);
+    filtrarTarjetasPorCategoria('Proyect'); //de comienzo se muestran primero las tarjetas con categoria 'proyect'
+});
